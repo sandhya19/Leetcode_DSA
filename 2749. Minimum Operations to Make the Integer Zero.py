00ddc8b62ecd51e81e34beaf20b674e7f065c2a3
Sandhya <sand.ash19@gmail.com>
2025-09-05 11:28:52 +0100

    add one POTD

---
+++ 2749. Minimum Operations to Make the Integer Zero.py
@@ -0,0 +1,7 @@
+def makeTheIntegerZero(self, num1: int, num2: int) -> int:
+        for k in range(1, 33):
+            curr = num1 - k * num2
+            if k >= curr.bit_count() and curr >= k:
+                return k
+
+        return -1
